ad78e2ae28724d6a2ad550d3a622394cdfea44e8
Ihar Mikailau <ihar.mikailau@ventionteams.com>
2025-09-19 22:58:26 +0200

    hotfix/fix-logos

--- src/components/ui/LoadingPlaceholder.tsx
+++ src/components/ui/LoadingPlaceholder.tsx
@@ -2,21 +2,9 @@ import { Box, Flex, Image } from '@chakra-ui/react';
 
 export function LoadingPlaceholder() {
   return (
-    <Flex
-      direction="column"
-      align="center"
-      justify="center"
-      minH="100vh"
-      bg="gray.50"
-      _dark={{ bg: "gray.900" }}
-    >
+    <Flex direction="column" align="center" justify="center" minH="100vh" bg="gray.50" _dark={{ bg: 'gray.900' }}>
       <Box mb={8}>
-        <Image
-          src="/Gemini_Generated_Image_fbnppgfbnppgfbnp.jpg"
-          alt="Test Analysis Portal"
-          maxW="300px"
-          h="auto"
-        />
+        <Image src="/image.png" alt="Test Analysis Portal" maxW="300px" h="auto" />
       </Box>
     </Flex>
   );
