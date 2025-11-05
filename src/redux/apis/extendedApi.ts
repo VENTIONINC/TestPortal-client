ca180316ee6b24adca18955a9bf034a10a1fd47a
Ihar Mikailau <ihar.mikailau@ventionteams.com>
2025-11-05 17:25:33 +0100

    fix/invalidate-issues

--- src/redux/apis/extendedApi.ts
+++ src/redux/apis/extendedApi.ts
@@ -3,35 +3,43 @@ import { BulkReviewRequest, BulkReviewResponse, GetResultsRequest, GetResultsRes
 import { generatedApi } from './generatedApi';
 import { TAGS } from './tags';
 
-export const extendedApi = generatedApi.injectEndpoints({
-  endpoints: (build) => ({
-    getResults: build.query<GetResultsResponse, GetResultsRequest>({
-      query: (params) => {
-        const filteredParams = Object.fromEntries(
-          Object.entries(params)
-            .map(([key, value]) => [key, typeof value === 'string' ? value.trim() : value])
-            .filter(([, value]) => value !== '' && value !== null && value !== undefined),
-        );
-
-        return {
-          url: '/api/v2/results',
-          params: filteredParams,
-          method: 'GET',
-        };
+export const extendedApi = generatedApi
+  .enhanceEndpoints({
+    endpoints: {
+      postApiV2Assumptions: {
+        invalidatesTags: [TAGS.Assumption, TAGS.Issues],
       },
-      providesTags: [TAGS.Result],
-    }),
-    bulkReview: build.mutation<BulkReviewResponse, BulkReviewRequest>({
-      query: ({ errorIds }) => ({
-        url: '/api/v2/result-errors/bulk-review',
-        method: 'PATCH',
-        body: { errorIds },
+    },
+  })
+  .injectEndpoints({
+    endpoints: (build) => ({
+      getResults: build.query<GetResultsResponse, GetResultsRequest>({
+        query: (params) => {
+          const filteredParams = Object.fromEntries(
+            Object.entries(params)
+              .map(([key, value]) => [key, typeof value === 'string' ? value.trim() : value])
+              .filter(([, value]) => value !== '' && value !== null && value !== undefined),
+          );
+
+          return {
+            url: '/api/v2/results',
+            params: filteredParams,
+            method: 'GET',
+          };
+        },
+        providesTags: [TAGS.Result],
       }),
-      invalidatesTags: [TAGS.Result],
+      bulkReview: build.mutation<BulkReviewResponse, BulkReviewRequest>({
+        query: ({ errorIds }) => ({
+          url: '/api/v2/result-errors/bulk-review',
+          method: 'PATCH',
+          body: { errorIds },
+        }),
+        invalidatesTags: [TAGS.Result],
+      }),
     }),
-  }),
-  overrideExisting: false,
-});
+    overrideExisting: false,
+  });
 
 export const {
   usePostApiV2AssumptionsMutation: useCreateAssumptionMutation,
