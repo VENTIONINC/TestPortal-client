34643f86f25a491cd89cea37ccab59098af3294f
Ihar Mikailau <ihar.mikailau@ventionteams.com>
2025-12-16 18:02:12 +0100

    chore/update-infra-naming: Update 'Infra' issue category naming to 'Environment'

--- src/utils/issue-category.ts
+++ src/utils/issue-category.ts
@@ -5,14 +5,14 @@ import { IssueCategory } from '@/types';
 export const getIssueCategoryStyle = (category: IssueCategory) => {
   switch (category) {
     case IssueCategory.Bug:
-      return { Icon: LuBug, color: 'red.500', hoverBgColor: 'red.200' };
+      return { Icon: LuBug, color: 'red.500', hoverBgColor: 'red.200', name: category };
     case IssueCategory.Script:
-      return { Icon: LuCode, color: 'yellow.600', hoverBgColor: 'yellow.200' };
+      return { Icon: LuCode, color: 'yellow.600', hoverBgColor: 'yellow.200', name: category };
     case IssueCategory.Infra:
-      return { Icon: LuServer, color: 'blue.500', hoverBgColor: 'blue.200' };
+      return { Icon: LuServer, color: 'blue.500', hoverBgColor: 'blue.200', name: 'Environment' };
     case IssueCategory.Performance:
-      return { Icon: LuCpu, color: 'orange.500', hoverBgColor: 'orange.200' };
+      return { Icon: LuCpu, color: 'orange.500', hoverBgColor: 'orange.200', name: category };
     default:
-      return { Icon: LuCircleHelp, color: 'gray.500', hoverBgColor: 'gray.200' };
+      return { Icon: LuCircleHelp, color: 'gray.500', hoverBgColor: 'gray.200', name: category };
   }
 };
